d6a48c8627d37fc058ef9ec08d40fb018dcdd17e
SergioCardenasP <80847593+SergioCardenasP@users.noreply.github.com>
2023-12-02 15:05:56 -0600

    Update hello.py to meet Pylint requirements

--- hello.py
+++ hello.py
@@ -1,7 +1,4 @@
-def add(x, y):
-    return x + y
-
-
-x, y = 5, 3
-
-print(f"This is the sum of {x} and {y}: {add(x,y)} ")
+def add(x,y):
+    return x+y
+    
+print(f"This is the sum of {5} and {3}: {add(5,3)} ")
